f99524ae58c687452be344f2ef77cf9ed7e351c8
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-04-04 20:46:11 +0530

    .

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -5,6 +5,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     // clear state
